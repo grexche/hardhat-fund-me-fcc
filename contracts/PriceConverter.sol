5480eb2dbaecca7bf5766260e01c2f803347a08d
grexche <grexche@gmail.com>
2022-12-25 01:13:25 +0800

    initial commit

---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice(
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 1,000.00000000
+        return uint256(answer * 1e10);
+    }
+
+    function getConversionRate(
+        uint256 ethAmount,
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice(priceFeed);
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
